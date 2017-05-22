aff3ff9f5b1d367834ed1fe6f86d9e6eaf268d2a
Nguyen Dinh Trinh <ndtrinh.qb>
2017-05-22 13:40:01 +0700

    add entity framework code first for data

---
+++ TeduShop/TeduShop.Model/Abstract/ISeoable.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface ISeoable
+    {
+        [MaxLength(256)]
+        string MetaKeyword { get; set; }
+
+        [MaxLength(256)]
+        string MetaDescription { get; set; }
+    }
+}
