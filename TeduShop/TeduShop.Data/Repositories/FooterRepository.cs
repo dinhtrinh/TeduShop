34ce0d3f0c4d3105f983948cebb3d43154e96d2b
Nguyen Dinh Trinh <ndtrinh.qb>
2017-05-22 19:24:01 +0700

    Update repositories table

--- TeduShop/TeduShop.Data/Repositories/FooterRepository.cs
+++ TeduShop/TeduShop.Data/Repositories/FooterRepository.cs
@@ -8,13 +8,13 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public interface IFooterRepository
+    public interface IFooterRepository : IRepository<Footer>
     {
 
     }
     public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
     {
-        public FooterRepository(DbFactory dbFactory) 
+        public FooterRepository(DbFactory dbFactory)
             : base(dbFactory)
         {
 
